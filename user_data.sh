ef4f67734c61a0fae6f98e3bb80d38b9df3d5798
Elias_Dias <75236438+Elias-Dias-De-Araujo@users.noreply.github.com>
2023-10-23 17:25:49 -0300

    fix: instalação do docker-compose

--- user_data.sh
+++ user_data.sh
@@ -1,24 +1,12 @@
 #!/bin/bash
-
-# Atualizando o sistema e instalando e habilitando o docker
+yum update -y
 yum install -y docker
 systemctl start docker
 systemctl enable docker
-
-# Download do dockerfile com wordpress
-wget "a definir"
-
-# Criando imagem docker a partir do dockerfile
-docker build -t wordpress-image -f Dockerfile.
-
-# Criando container wordpress
-docker run -d --name my-wordpress-container -p 80:80 my-wordpress-image
-
-# Instalando o cliente MySQL
-yum install -y mysql
-
-# Configurar o WordPress para se conectar ao banco de dados RDS MySQL
-docker exec -it my-wordpress-container bash -c "yum install -y mysql"
-docker exec -it my-wordpress-container wp config create --dbname=DB_NAME --dbuser=DB_USER --dbpass=DB_PASSWORD --dbhost=DB_HOST
-
-...
+curl -L "https://github.com/docker/compose/releases/latest/download/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+chmod +x /usr/local/bin/docker-compose
+# Esta linha de curl do yml não está funcionando adequadamente
+#curl -L "https://github.com/Elias-Dias-De-Araujo/pratica_docker_compass_uol/blob/main/docker-compose.yml" -o /home/ec2-user/docker-compose.yml
+usermod -aG docker ${USER}
+chmod 666 /var/run/docker.sock
+docker-compose -f /home/ec2-user/docker-compose.yml up -d
