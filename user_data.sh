b85caea251e4b5b892c0ac2f36ecb76955bea770
Elias_Dias <75236438+Elias-Dias-De-Araujo@users.noreply.github.com>
2023-10-24 14:55:31 -0300

    fix: script v1

--- user_data.sh
+++ user_data.sh
@@ -6,6 +6,11 @@ systemctl enable docker
 curl -L "https://github.com/docker/compose/releases/latest/download/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 chmod +x /usr/local/bin/docker-compose
 curl -sL "https://raw.githubusercontent.com/Elias-Dias-De-Araujo/pratica_docker_compass_uol/main/docker-compose.yaml" --output "/home/ec2-user/docker-compose.yaml"
+yum install nfs-utils -y
+mkdir /mnt/efs/
+chmod +rwx /mnt/efs/
+mount -t nfs4 -o nfsvers=4.1,rsize=1048576,wsize=1048576,hard,timeo=600,retrans=2,noresvport fs-07c68e847f4ea9744.efs.us-east-1.amazonaws.com:/ /mnt/efs/
+echo "fs-07c68e847f4ea9744.efs.us-east-1.amazonaws.com:/ /mnt/efs nfs defaults 0 0" >> /etc/fstab
 usermod -aG docker ${USER}
 chmod 666 /var/run/docker.sock
-docker-compose -f /home/ec2-user/docker-compose.yml up -d
+docker-compose -f /home/ec2-user/docker-compose.yaml up -d
