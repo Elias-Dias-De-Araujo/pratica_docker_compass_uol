1e0f563e181f1fc410d9a0c0ff0ed15f60801cd7
Elias_Dias <75236438+Elias-Dias-De-Araujo@users.noreply.github.com>
2023-10-24 13:51:29 -0300

    Update user_data.sh

--- user_data.sh
+++ user_data.sh
@@ -5,8 +5,7 @@ systemctl start docker
 systemctl enable docker
 curl -L "https://github.com/docker/compose/releases/latest/download/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 chmod +x /usr/local/bin/docker-compose
-curl -sL "https://raw.githubusercontent.com/Elias-Dias-De-Araujo/pratica_docker_compass_uol/m
-ain/docker-compose.yaml" --output "/home/ec2-user/docker-compose.yaml"
+curl -sL "https://raw.githubusercontent.com/Elias-Dias-De-Araujo/pratica_docker_compass_uol/main/docker-compose.yaml" --output "/home/ec2-user/docker-compose.yaml"
 usermod -aG docker ${USER}
 chmod 666 /var/run/docker.sock
 docker-compose -f /home/ec2-user/docker-compose.yml up -d
